437be6dda6515f194c3bf397480dbfd45c1514dc
bhr597 <bhr597@mjolnirgate01fl.unicph.domain>
2026-01-20 13:36:02 +0100

    edited batch scripts

--- Fit_HMSC_Model.sh
+++ Fit_HMSC_Model.sh
@@ -1,31 +1,34 @@
 #!/bin/bash
-#SBATCH --job-name=Tensorflow
-#SBATCH  -M ukko
-#SBATCH --partition=gpu 
-#SBATCH --mem-per-cpu=4G
+#SBATCH --job-name=hmsc-trial
+#SBATCH --partition=gpuqueue
+#SBATCH --cpus-per-gpu=16
+#SBATCH --mem-per-cpu=1GB
 #SBATCH --gpus-per-node=1
-#SBATCH --cpus-per-gpu=2
-#SBATCH --time=00:30:00
+#SBATCH --time=00:00:05
 #SBATCH --array=0-3
-#SBATCH --output=R-%x.%j.out
+#SBATCH --mail-type=END # Mail events (NONE, BEGIN, END, FAIL, ALL)
+#SBATCH --mail-user=bhr597@sund.ku.dk
 
-module purge
-module load Python cuDNN
-source $HOME/myenv/bin/activate
+# Load Conda into the shell
+module restore myhmscstack
+source /opt/software/anaconda3/2021.05/etc/profile.d/conda.sh
+conda activate my_hmsc_env
 
+### DATA PATH
+Area='Hmsc Outputs'
 model_name=$NAME
+data_path=$(printf "%s/%s" $Area $model_name)
 
-Area=$AREA
+### WHICH MODEL 
 samples=$SAMP
 thin=$THIN
 chains=4
 verbose=100
-divide=$samples*$thin; by=2; (( transient=(divide+by-1)/by ))
-data_path=$(printf "%s/%s" $Area $model_name)
+transient=100000
 screen_text=$(printf "#######\n Model Run starting\n Samples: %.4d\n Thining: %.2d Transient steps: %.d\n######\n" $samples $thin $transient)
 
-input_path=$data_path/$(printf "INIT/HPC_INIT_samples_%.4d_thin_%.2d_chains_%.1d.rds" $samples $thin $chains)
-output_path=$data_path/$(printf "Sampled/HPC_samples_%.4d_thin_%.2d_chain_%.1d.rds" $samples $thin $SLURM_ARRAY_TASK_ID)
+input_path=$data_path/$(printf "Models/INIT/HPC_INIT_samples_%.4d_thin_%.2d_chains_%.1d.rds" $samples $thin $chains)
+output_path=$data_path/$(printf "Models/Sampled/HPC_samples_%.4d_thin_%.2d_chain_%.1d.rds" $samples $thin $SLURM_ARRAY_TASK_ID)
 
 echo $screen_text
 echo $data_path
